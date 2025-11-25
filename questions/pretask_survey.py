a8ae571fd4cafa47f5f33bb0bcdc9045553b9549
Zhiyan Bo <47777260+ZhiyanBo@users.noreply.github.com>
2025-11-25 12:31:38 +0000

    Update clinical expertise question 1

--- questions/pretask_survey.py
+++ questions/pretask_survey.py
@@ -5,7 +5,7 @@
 clinical_expertise_questions = [
     {
     "question_number": 0, 
-    "question": "Are you a rheumatologist or a radiologist?",
+    "question": "Are you a rheumatologist, radiologist or researcher?",
     "options": ["Rheumatologist", 
                 "Radiologist",
                 "Researcher"],
